f87ae36684b3c082c70670269e21f73b99345c56
timidENFP <112996872+timidENFP@users.noreply.github.com>
2022-09-21 10:55:31 +0900

    2주차1번문제
    -한글 택스처가 께져서 다시올림.
    -업로드후 cpp파일이 부제로 다시올림.
    - 2과(55pp) 문제임

---
+++ 2end_week/ex-1/ex-1/소스.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+	printf("student ID : % d\n", 2019010831);
+
+	printf("name : %s\n", "young woo lee");
+
+	printf("grade : %c\n", 'B');
+
+	return 0;
+
+}
